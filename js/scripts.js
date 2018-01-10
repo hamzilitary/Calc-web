2f5565823cbffd84186a4fd4266a55d9ba54a987
Guest User <Guest@Epicodus-10F.local>
2018-01-10 11:44:13 -0800

    added subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,19 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
+    var someInput = $("input#some-input").val();
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
     $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    var someInput = $("input#some-input").val();
+   event.preventDefault();
+   var number1 = parseInt($("#subtract1").val());
+   var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $(".output").text(result);
   });
 });
